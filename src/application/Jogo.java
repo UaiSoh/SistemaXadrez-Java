d5b955313cf54a697a3d4da4528795f0e7c5f266
João Pedro <joaopedroh101@gmail.com>
2024-06-08 17:28:10 -0300

    Projeto Iniciado

---
+++ src/application/Jogo.java
@@ -0,0 +1,14 @@
+package application;
+
+import mesajogo.Posicao;
+
+public class Jogo {
+
+	public static void main(String[] args) {
+		
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
+
+	}
+
+}
